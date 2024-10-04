17fd6a83a462bf195f49ac9600b4e13c827f3eb6
Someet-Git <someetsahoo654@gmail.com>
2024-10-04 22:23:16 +0530

    added model trainer

--- DroneDetection/pipeline/training_pipeline.py
+++ DroneDetection/pipeline/training_pipeline.py
@@ -3,18 +3,21 @@ from DroneDetection.logger import logging
 from DroneDetection.exception import AppException
 from DroneDetection.components.data_ingestion import DataIngestion
 from DroneDetection.components.data_validation import DataValidation
-# from DroneDetection.components.model_trainer import ModelTrainer
+from DroneDetection.components.model_trainer import ModelTrainer
 
 from DroneDetection.entity.config_entity import (DataIngestionConfig,
-                                                 DataValidationConfig)
+                                                 DataValidationConfig,
+                                                 ModelTrainerConfig)
+
 from DroneDetection.entity.artifacts_entity import (DataIngestionArtifact,
-                                                    DataValidationArtifact)
+                                                    DataValidationArtifact,
+                                                    ModelTrainerArtifact)
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
-
+        self.model_trainer_config = ModelTrainerConfig()
     
 
     def start_data_ingestion(self)-> DataIngestionArtifact:
@@ -64,14 +67,34 @@ class TrainPipeline:
 
         except Exception as e:
             raise AppException(e, sys) from e
-        
+
+
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise AppException(e, sys)    
+
+
 
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(
                 data_ingestion_artifact = data_ingestion_artifact
-                )
+            )
+            
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
+            
+            else:
+                raise Exception("Your data is not in correct format")
 
         except Exception as e:
             raise AppException(e, sys)
--- DroneDetection/utils/main_utils.py
+++ DroneDetection/utils/main_utils.py
@@ -3,8 +3,8 @@ import sys
 import yaml
 import base64
 
-from wasteDetection.exception import AppException
-from wasteDetection.logger import logging
+from DroneDetection.exception import AppException
+from DroneDetection.logger import logging
 
 
 def read_yaml_file(file_path: str) -> dict:
